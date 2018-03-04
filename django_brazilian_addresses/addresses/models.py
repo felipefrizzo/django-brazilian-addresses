6d87145b385d7c4aa7b4c18e9d2bac248984b17e
Felipe Frizzo <felipefrizzo@gmail.com>
2018-03-03 23:02:22 -0300

    Create state model and migration

--- django_brazilian_addresses/addresses/models.py
+++ django_brazilian_addresses/addresses/models.py
@@ -10,3 +10,16 @@ class Country(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class State(models.Model):
+    name = models.CharField('name', max_length=255)
+    country = models.ForeignKey(
+        'Country', on_delete=models.CASCADE, verbose_name='country')
+    created_at = models.DateTimeField(
+        'created at', auto_now_add=True, auto_now=False)
+    updated_at = models.DateTimeField(
+        'updated at', auto_now_add=True, auto_now=False)
+
+    def __str__(self):
+        return self.name
