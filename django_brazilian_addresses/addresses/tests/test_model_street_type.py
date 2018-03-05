c9063008df8ef7b79ebb4de8ca2cd762b43fe81d
Felipe Frizzo <felipefrizzo@gmail.com>
2018-03-05 20:09:43 -0300

    Create tests for street type model

---
+++ django_brazilian_addresses/addresses/tests/test_model_street_type.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from django.test import TestCase
+
+from django_brazilian_addresses.addresses.models import StreetType
+
+
+class StreetTypeModelTest(TestCase):
+    def setUp(self):
+        self.street_type = StreetType.objects.create(name='Rua')
+
+    def test_create(self):
+        self.assertTrue(StreetType.objects.exists())
+
+    def test_str(self):
+        self.assertEqual('Rua', str(self.street_type))
+    
+    def test_abbreviation_blank(self):
+        """Abbreviation code can be blank"""
+        field = StreetType._meta.get_field('abbreviation')
+        self.assertTrue(field.blank)
+
+    def test_abbreviation_null(self):
+        """Abbreviation code can be null"""
+        field = StreetType._meta.get_field('abbreviation')
+        self.assertTrue(field.null)
+
+    def test_create_date(self):
+        """Street Type must have an auto created_at attrs."""
+        self.assertIsInstance(self.street_type.created_at, datetime)
+
+    def test_update_date(self):
+        """Street Type must have an auto updated_at attrs."""
+        self.assertIsInstance(self.street_type.updated_at, datetime)
+
