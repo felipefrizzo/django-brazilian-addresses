d15ba95946d9c15bcaa268eaedaac43b426170a6
Felipe Frizzo <felipefrizzo@gmail.com>
2018-04-03 11:42:11 -0300

    Create city view and url

--- django_brazilian_addresses/addresses/urls.py
+++ django_brazilian_addresses/addresses/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from django_brazilian_addresses.addresses.views import CountryView, StateView
+from django_brazilian_addresses.addresses.views import CountryView, \
+    StateView, CityView
 
 router = routers.SimpleRouter()
 router.register(r'countries', CountryView)
 router.register(r'states', StateView)
+router.register(r'cities', CityView)
 
 urlpatterns = [
     path('', include(router.urls))
--- django_brazilian_addresses/addresses/views.py
+++ django_brazilian_addresses/addresses/views.py
@@ -1,8 +1,8 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
-from django_brazilian_addresses.addresses.models import Country, State
+from django_brazilian_addresses.addresses.models import Country, State, City
 from django_brazilian_addresses.addresses.serializers import \
-    CountrySerializer, StateSerializer
+    CountrySerializer, StateSerializer, CitySerializer
 
 
 class CountryView(ReadOnlyModelViewSet):
@@ -15,3 +15,8 @@ class StateView(ReadOnlyModelViewSet):
     queryset = State.objects.all()
     serializer_class = StateSerializer
     pagination_class = None
+
+
+class CityView(ReadOnlyModelViewSet):
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
